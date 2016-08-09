86992bed44dd45bcdef5c67ba72d2ddcbaead721
Emeric Henon <emeric.henon@gmail.com>
2016-08-09 17:20:55 +0200

    exo 2 bien en cours d OAuth

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809131445) do
+ActiveRecord::Schema.define(version: 20160809151217) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
